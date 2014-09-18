b8e6a64a34319a1e03e7b0fe4242d166adf5d6fe
inotaku <i_no_u_u@yahoo.co.jp>
2014-09-18 19:10:23 +0900

    新規作成

---
+++ Blogbook/src/main/java/jp/com/inotaku/domain/Blog.java
@@ -0,0 +1,43 @@
+package jp.com.inotaku.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="blog")
+public class Blog {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="blog_id")
+	private long blogId;
+
+	@Column(name="title")
+	private String title;
+	
+	@Column(name="message")
+	private String message;
+	
+	public long getBlogId() {
+		return blogId;
+	}
+	public void setBlogId(long blogId) {
+		this.blogId = blogId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
