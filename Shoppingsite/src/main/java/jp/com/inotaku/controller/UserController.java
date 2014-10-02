0f8c975fde9d16a6fc264cbf3b4c60ed7ea9e906
inotaku <i_no_u_u@yahoo.co.jp>
2014-10-02 13:57:28 +0900

    ユーザー登録機能

--- Shoppingsite/src/main/java/jp/com/inotaku/controller/UserController.java
+++ Shoppingsite/src/main/java/jp/com/inotaku/controller/UserController.java
@@ -3,6 +3,7 @@ package jp.com.inotaku.controller;
 import javax.validation.Valid;
 
 import jp.com.inotaku.domain.User;
+import jp.com.inotaku.domain.User.userEntry;
 import jp.com.inotaku.service.UserService;
 import jp.com.inotaku.validation.Login;
 import jp.com.inotaku.validation.LoginForm;
@@ -21,26 +22,44 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String toLoginView(Model model){
-		model.addAttribute("user",new User());
+	public String toLoginView(Model model) {
+		model.addAttribute("user", new User());
 		return "login";
 	}
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String onSubmit(@Validated(LoginForm.class) User user,BindingResult bindingResult, Model model){
-		
-		if(bindingResult.hasErrors()){
+	public String onSubmit(@Validated(LoginForm.class) User user,
+			BindingResult bindingResult, Model model) {
+
+		if (bindingResult.hasErrors()) {
 			return "login";
 		}
-		
+
 		try {
-			User loginUser = userService.getUserByUserNameAndPassword(user.getUserName(), user.getPassword());
-			model.addAttribute("loginUser",loginUser);
+			User loginUser = userService.getUserByUserNameAndPassword(
+					user.getUserName(), user.getPassword());
+			model.addAttribute("loginUser", loginUser);
 		} catch (Exception e) {
 			return "login";
 		}
 		return "loginSuccess";
+	}
+
+	@RequestMapping(value = "userEntry", method = RequestMethod.GET)
+	public String toUserEntryView(Model model) {
+		model.addAttribute("user", new User());
+		return "userEntry";
+	}
+
+	@RequestMapping(value = "userEntry", method = RequestMethod.POST)
+	public String addUser(@Validated(userEntry.class) User user, BindingResult bindingResult, Model model) {
+		if(bindingResult.hasErrors()){
+			return "userEntry";
+		}
+		userService.addUser(user);
+		model.addAttribute("user",user);
+		return "userEntrySuccess";
 	}
 }
