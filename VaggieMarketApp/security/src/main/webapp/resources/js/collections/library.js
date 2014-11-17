e3265ca43f79aa4993e5707f560432049a5c584c
inotaku <i_no_u_u@yahoo.co.jp>
2014-11-17 20:11:03 +0900

    修正

--- VaggieMarketApp/security/src/main/webapp/resources/js/collections/library.js
+++ VaggieMarketApp/security/src/main/webapp/resources/js/collections/library.js
@@ -2,5 +2,5 @@ var app = app || {};
 
 app.Library = Backbone.Collection.extend({
     model: app.Blog,
-    url:"http://localhost:8090/Blogbook/json/"
+    url:"http://localhost:8080/security/json/"
 });
