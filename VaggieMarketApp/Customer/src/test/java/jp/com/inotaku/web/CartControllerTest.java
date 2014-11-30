84dcbb926adf4ba701ef9ce4851e872f0bfebbc4
inotaku <i_no_u_u@yahoo.co.jp>
2014-11-30 17:15:26 +0900

    mockmvcテスト追加

---
+++ VaggieMarketApp/Customer/src/test/java/jp/com/inotaku/web/CartControllerTest.java
@@ -0,0 +1,55 @@
+package jp.com.inotaku.web;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.ModelResultMatchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+		"file:src/main/resources/spring/application-config.xml",
+		"file:src/main/webapp/WEB-INF/mvc-config.xml" })
+@WebAppConfiguration
+public class CartControllerTest {
+	
+	private MockMvc mockMvc;
+	
+	@InjectMocks
+	private CartController cartController;;
+	
+
+	@Before
+	public void setUp() {
+		
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/view/");
+        viewResolver.setSuffix(".jsp");
+		MockitoAnnotations.initMocks(this);
+		mockMvc = standaloneSetup(cartController).setViewResolvers(viewResolver)
+				.build();
+	}
+	
+	@Test
+	public void testRegisterGet() throws Exception {
+		MvcResult mvcResult = mockMvc.perform(get("/cart"))
+				.andExpect(status().isOk()).andExpect(view().name("cart"))
+				.andReturn();
+	}
+}
